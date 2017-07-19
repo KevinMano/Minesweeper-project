6ec9d81fa82383b09bc84784d9da5bb2c0cbdda1
Kevin Mano <kevin_rotijohn@yahoo.com>
2017-07-18 23:05:44 -0400

    Update My_Minesweeper.py

--- My_Minesweeper.py
+++ My_Minesweeper.py
@@ -9,9 +9,33 @@ from Tkinter import *
 import random
 import time
 
+'''
+State_array encodes the states of the board for the presence of mines. 
+There are 2 states for State_array: Mine or No Mine.
+
+Besides that, we also have the button array, which encodes the buttons in a grid.
+This array has three states: flagged, open or closed.
+
+To capture the logic of each cell, consider below:
+IF(OPENED && CONTAINS_MINE) --> BOOOOOOMMMMMM!!!! 
+IF(OPENED && !(CONTAINS_MINE)) --> Evaluate neighbours, and place the number
+of mines contained in neighbours in the cell.
+
+IF(CLOSED && CONTAINS_MINE) --> NOTHING
+IF(CLOSED && !(CONTAINS_MINE)) --> NOTHING
+
+IF(FLAGGED && CONTAINS_MINE) -->
+IF(FLAGGED && !(CONTAINS_MINE)) -->
+
+So we need to encode this using both the arrays.
+Basically, this means to reference both arrays, and return the appropriate result.
+'''
+
+
 #Minesweeper_board class, containing the methods and attributes for the board.
 class Minesweeper_board:
     MineCount = 0
+    MoveCount = 0 
 
     #Initial screen, hit start to populate the board.
     def __init__(self, master, mines_no):
@@ -19,12 +43,32 @@ class Minesweeper_board:
         State_array = []
         Main_frame = Frame(master, width=576, height=768)
         Main_frame.pack()
-        self.button = Button(Main_frame, text='START', width=25, command = self.populate(State_array))
-        self.button.pack()
+        self.start_button = Button(Main_frame, text='START', width=25, command = lambda: self.draw(Main_window,State_array))
+        self.start_button.pack()
 
-    #create the mines and populate the state array, mines are given an X, the stuff around them need to be populated now too.
-    def populate(self, State_array):
-        State_array = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+    #In the first button press in minesweeper, there are no mines yet, so populate only happens after the first click on the board.
+    #This function simply sets up the whole board, starts the timer. After the first press, the populate function occurs.
+    #After that, this function continues with the populate. 
+    def draw(self,master,State_array):
+        self.start_button.destroy()
+        if self.MoveCount < 1:
+            State_array = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+            self.button = [[0 for columns in range(8)] for rows in range(8)]
+            for rows in range(0,8):
+                for columns in range(0,8):
+                    self.button[rows][columns] = Button(Main_window, text="", width=5, command=lambda i=rows, j=columns: self.populate(Main_window,State_array,i,j))
+                    self.button[rows][columns].grid(row=rows, column=columns)
+            self.MoveCount += 1
+            print self.MoveCount
+        else:
+            print "We made it here instead, still really good!"
+            self.MoveCount += 1
+            print self.MoveCount
+    
+    #create the mines and populate the state array, mines are given an X, the cells around the mines need to be populated now too.
+    #Remember, the first cell that is clicked cannot have a mine.
+    def populate(self, master, State_array,row,column):
+        self.button[row][column].config(relief = SUNKEN)
         Mine_count_temp = Minesweeper_board.MineCount                      
         for each in range(0,9,1):
             mine_row = random.randint(0,8)
@@ -33,6 +77,12 @@ class Minesweeper_board:
             New_mine = Mine(mine_row+1, mine_column+1)
             Mine_count_temp = Mine_count_temp - 1
         print State_array
+        self.button[row][column].set()
+
+    #Uncover the button, and do the check based on the logic above.
+    def Uncover(self, State_array, row, column):
+        print "We made it here!"
+
         
 #Mine class
 class Mine:
